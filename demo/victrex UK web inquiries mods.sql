bceef726dc70bdefb387e396f4b61a2cf088575f
Adam Plante <aplante@allwebcafe.com>
2012-09-24 18:12:48 -0400

    Original files from http://code.google.com/p/wsdldocument/. This was version 0.6

---
+++ demo/victrex UK web inquiries mods.sql
@@ -0,0 +1,34 @@
+INSERT INTO formRecipients(countryDB_DISPLAY_ONLY, countryID, formName, email, delimiter)
+VALUES
+("UK", 75, "ask-the-expert", "siwright@victrex.com", "vicote"),
+("UK", 75, "ask-the-expert", "lorr@victrex.com", "vicote"),
+("UK", 75, "ask-the-expert", "lorr@victrex.com", "aptiv"),
+("UK", 75, "ask-the-expert", "siwright@victrex.com", "aptiv"),
+("UK", 75, "ask-the-expert", "jsalisbury@victrex.com", "aptiv"),
+
+("Ireland", 35, "ask-the-expert", "siwright@victrex.com", "vicote"),
+("Ireland", 35, "ask-the-expert", "lorr@victrex.com", "vicote"),
+("Ireland", 35, "ask-the-expert", "lorr@victrex.com", "aptiv"),
+("Ireland", 35, "ask-the-expert", "siwright@victrex.com", "aptiv"),
+("Ireland", 35, "ask-the-expert", "jsalisbury@victrex.com", "aptiv"),
+
+("Northern Ireland", 47, "ask-the-expert", "siwright@victrex.com", "vicote"),
+("Northern Ireland", 47, "ask-the-expert", "lorr@victrex.com", "vicote"),
+("Northern Ireland", 47, "ask-the-expert", "lorr@victrex.com", "aptiv"),
+("Northern Ireland", 47, "ask-the-expert", "siwright@victrex.com", "aptiv"),
+("Northern Ireland", 47, "ask-the-expert", "jsalisbury@victrex.com", "aptiv"),
+
+("Wales", 78, "ask-the-expert", "siwright@victrex.com", "vicote"),
+("Wales", 78, "ask-the-expert", "lorr@victrex.com", "vicote"),
+("Wales", 78, "ask-the-expert", "lorr@victrex.com", "aptiv"),
+("Wales", 78, "ask-the-expert", "siwright@victrex.com", "aptiv"),
+("Wales", 78, "ask-the-expert", "jsalisbury@victrex.com", "aptiv"),
+
+("Scotland", 60, "ask-the-expert", "siwright@victrex.com", "vicote"),
+("Scotland", 60, "ask-the-expert", "lorr@victrex.com", "vicote"),
+("Scotland", 60, "ask-the-expert", "lorr@victrex.com", "aptiv"),
+("Scotland", 60, "ask-the-expert", "siwright@victrex.com", "aptiv"),
+("Scotland", 60, "ask-the-expert", "jsalisbury@victrex.com", "aptiv");
+
+
+
